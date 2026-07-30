b279db1fae72a41de2104bf3c7076aee9f00f0b7
Abdullah-Safi-8656 <safiabdullah769@example.com>
2026-07-30 03:22:06 -0700

    Feat: add Sticky navbar using Intersection Api.

--- script.js
+++ script.js
@@ -111,5 +111,22 @@ const handleHover = function(e) {
 
 // passing arguments into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
-
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky Nav using IntersectionObserver API
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function(entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`
+  }
+);
+
+headObserver.observe(header);
